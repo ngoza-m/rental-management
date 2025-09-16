1c366b2d51a2e13511ecd25bd9f961acbd50dfc0
Ngoza Mayaka <ngozamay@gmail.com>
2025-09-16 19:28:59 +0200

    lease model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  enum role: { super_admin: 0, landlord: 1, tenant: 2 }
+
   has_many :properties, foreign_key: :landlord_id
   has_many :tenants, foreign_key: :landlord_id
   has_many :leases, through: :tenants
